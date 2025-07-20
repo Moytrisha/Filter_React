b0bd021f16fe37e0ecb556b8e15abb3b2ffb4ca9
Moytrisha <swetabhattacharya925@gmail.com>
2025-07-20 13:20:58 +0530

    green tick will come on clicking on task

--- src/Component/Form/Form.jsx
+++ src/Component/Form/Form.jsx
@@ -6,9 +6,12 @@ function Form (){
     const [isedit,setIsedit] = useState([])
     const [select,setSelect] = useState("")
     const [editValue, setEditValue]=useState("");
+    const greentick = "https://media.istockphoto.com/id/691856234/vector/flat-round-check-mark-green-icon-button-tick-symbol-isolated-on-white-background.jpg?s=612x612&w=0&k=20&c=hXL5nXQ2UJlh4yzs2LyZC4GtctQG0fs-mk30GPPbhbQ="
+    const [flag,setFlag] = useState([])
     function Add (e){
         setList([...list,input])
         setIsedit([...isedit,false])
+        setFlag([...flag,false])
     }
 
     function del(index) {
@@ -53,11 +56,22 @@ function Form (){
         setList(updatedEdit);
         edit(index,false)
     }
-    // function Dropdown() {
-    //     for (let i=0;i<list[i].length;i++) {
 
-    //     }
-    // }
+    function tick (index) {
+        let update =[]
+        for(let i =0;i<flag.length;i++) {
+            if(i===index) {
+                update.push(!flag[i])
+            }
+
+            else {
+                update.push(flag[i])
+            }
+        }
+
+        setFlag(update)
+    }
+    
     return (
         <div className="to-do">
             <input type="text" onChange={(e)=>{setInput(e.target.value)}}/><button onClick={()=>Add()}>Add</button>
@@ -65,9 +79,10 @@ function Form (){
             {list.map((item,i)=>item?.includes(select) && 
             <div>{isedit[i]!=true ?
                 <span>
-                    <span>{item}</span>
+                    <span onClick={()=>tick(i)}>{item}</span>
                     <button onClick={()=>del(i)}>Delete</button>
                     <button onClick={()=>edit(i,true)}>Edit</button>
+                    {flag[i] && <img src={greentick} />}
                 </span>
                 :
                 <span>
